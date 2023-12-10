0507d868723420fd86d4737467b94400a328f5a1
Vishnupirya A <66973470+Vishnupriya-Anupindi@users.noreply.github.com>
2023-12-10 17:13:37 +0100

    test implemented

--- Col_red_utils.jl
+++ Col_red_utils.jl
@@ -1,3 +1,4 @@
+using LinearAlgebra
 using Test
 
 @inline function norm_coord(v, b, bf = float(b))
@@ -10,8 +11,20 @@ end
 
 @test norm_coord([1 1 0 1],2) == 13/16
 
-function compute_P_j(C, A, w, b, m, s, st, τ)
+function red_mat(C,w)
+    CT = deepcopy(C)
+    for i in eachindex(w)
+        for j in 1:w[i]
+            CT[i][:,end-j+1] .= 0  
+        end  
+    end
+    return CT
+end
 
+# CT[i][:,end-w[i]+1 : end] .= 0 replace this instead of the j for loop
+
+function compute_P_j(P, A, w, st, τ)
+(;C,b,m,s) = P
     P_j = zeros(1,τ)
     for j in st:-1:1
         # computing X_j
@@ -20,129 +33,73 @@ function compute_P_j(C, A, w, b, m, s, st, τ)
             Cn = (C[j]*(digits(i-1, base=b, pad=m))) .% b
             X_j[i] = norm_coord(Cn,b)
         end
-        q_j = X_j*A[j,:]
+        q_j = X_j*A[j:j,:]
 
         # computing P_j
-        n_w = min((w[j+1]) , m) - w[j]
+        n_w = min( get(w,j+1,m) , m) - w[j]
         P_j = repeat(P_j,b^n_w) + q_j
-        @show(size(P_j))
-        @show n_w
+        # @show size(P_j) n_w q_j
+        
     end
     return P_j
 end
 
-compute_P_j(0,0,0,[2,2,3,5],2,5,4,3,5)
-
-
 
 # pts needs to be of size (s, N) where N is length(badic) = b^m
-function get_points!(pts, C, badic, b, m,bf = float(b))  
+function get_points!(pts, P) 
+    (;C,b,m,s) = P  
     Cn = zeros(Int64, m) 
     @inbounds for j in eachindex(pts)  # 1:N
-        n = badic[j]
+        n = digits(j-1, base=b, pad=m)
 
         for i in eachindex(C)
             mul!(Cn, C[i], n)
             for k in eachindex(Cn)
-                Cn[k] = Cn[k] % b
+                Cn[k] = Cn[k] % b  # @. Cn = Cn %b
             end 
-            pts[j][i] = norm_coord(Cn, b, bf)
+            pts[j][i] = norm_coord(Cn, b)
         end
     end
 end
 
-function get_points(C, badic, b, m, s, bf = float(b))  
-    pts = [zeros(s) for i in 1:length(badic)]
-    get_points!(pts, C, badic, b, m, bf)
+function get_points(P) 
+    (;C,b,m,s) = P 
+    pts = [zeros(s) for i in 1:b^m]
+    get_points!(pts, P)
     return pts
 end
 
-get_badic(b, m) = collect.(Iterators.product(fill(0:b-1, m)...))[:]
+@testset "column reduced computation" begin 
+    P = (C = ( [1 0; 0 1], [0 1; 1 0] ),
+        b = 2, m = 2,s = 2)
+    A = [1 2 3; 2 5 1]
+    w = [0,1]
+    st = 2
+    τ = 3
+    prod_alg = compute_P_j(P, A, w, st, τ)
+    
+    CT = red_mat(P.C,w)
+    PT = (;P...,C = CT)
+    X = stack(get_points(PT))'
+
+    @test prod_alg == X*A
+end
 
 @testset "get points" begin
     C = ( [1 0; 0 1], [0 1; 1 0] )
     b = 2
     m = 2
     s = 2
-    badic = get_badic(b,m)
-    bf = float(b)
-    pts = [zeros(s) for i in 1:length(badic)]
-    get_points!(pts, C, badic, b, m, bf)
+    P = (;C,b,m,s)
+    pts = get_points(P)
     @test pts == [[0.0, 0.0], [0.5, 0.25],[0.25; 0.5], [0.75, 0.75]]
     
 end
 
-@testset "get points" begin
-    C = ([2 1 1; 1 1 0; 1 0 0], [0 1 1; 1 1 0; 1 0 0])
-    b = 3
-    m = 3
-    s = 2
-    badic = get_badic(b,m)
-    bf = float(b)
-    pts = [zeros(s) for i in 1:length(badic)]
-    get_points!(pts, C, badic, b, m, bf)
-    @test pts == [0 0.25 0.5 0.75; 0 0.5 0.25 0.75]
-end
 
 
-# generates the ith matrix (returned as a vector) 
-# in base b with m rows and m columns 
-function int_to_matrix!(C, i, b, m) 
-    C .= 0
-    if i > 0
-        digits!(C, i, base=b)
-    end
-    return C
-end 
-
-function int_to_matrix(i, b, m) 
-    C = zeros(Int, m*m)
-    return int_to_matrix!(C, i, b, m)
-end 
-
-@test int_to_matrix(0, 3, 2) == [0, 0, 0, 0]
-@test int_to_matrix(5, 3, 2) == [2, 1, 0, 0]
-@test int_to_matrix(3^(2*2)-1, 3, 2) == [2, 2, 2, 2]
-
-
-
-get_matrix(i, b, m) = reshape(int_to_matrix(i, b, m), m, m)
-#get_matrices(i1,i2,b,m) = [ get_matrix(i1,b,m) get_matrix(i2,b,m) ]
-
-
-
-function compare_rows_matrix(C1,C2)
-    C = (C1,C2)
-    if C[1][1,:] == C[2][1,:]
-        return true
-    end
-
-    if C[1][1,:] == C[2][2,:]
-        return true
-    end
-
-    if C[1][2,:] == C[2][1,:]
-        return true
-    end
-    return false
-end
-
-function compare_rows_matrix(C1,C2,C3)
-    C = (C1,C2,C3)
-    for d1 in 1:3
-        for d2 in 1:3
-            if d1<d2 && C[d1][1,:] == C[d2][1,:]
-                return true
-            end
-
-            #if d1!=d2 && C[d1][1,:] == C[d2][2,:]
-            #    return true
-            #end
-        end
-    end
-    return false
-end
-
-
-
-#hshojhsfjkhsfjdlk
+   # C = ([2 1 1; 1 1 0; 1 0 0], [0 1 1; 1 1 0; 1 0 0])
+   # b = 3
+   # m = 3
+   # s = 2
+    
